0903978fcc629ce83117261abb284c84197a1950
Thiago de Matos <thiago.dematos.1@gmail.com>
2023-10-11 22:06:32 -0400

    created side item w/ css and added function for opening and closing modal

--- src/index.js
+++ src/index.js
@@ -8,3 +8,27 @@ class ToDo {
 class Project {
   constructor() {}
 }
+
+//function for opening modal
+function openModal() {
+  const addItem = document.querySelector("#add-todo");
+  const modal = document.querySelector("dialog");
+
+  addItem.addEventListener("click", () => {
+    modal.showModal();
+  });
+}
+
+openModal();
+
+//function for closing modal
+function closeModal() {
+  const closeModalbutton = document.querySelector(".cancel-button");
+  const modal = document.querySelector("dialog");
+
+  closeModalbutton.addEventListener("click", () => {
+    modal.close();
+  });
+}
+
+closeModal();
