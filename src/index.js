cd9501492a1e0d4aa0f3835086be160e90beb22b
Thiago de Matos <thiago.dematos.1@gmail.com>
2023-10-19 18:41:01 -0400

    create function for sorting the list and array of todos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-//create an array that stores my todo's
+// create an array that stores my todo's
 const toDoArray = [];
 
 class ToDo {
@@ -10,12 +10,12 @@ class ToDo {
   }
 }
 
-//create a function for pushing todos to array
+// create a function for pushing todos to array
 function addToDoToArray(toDo) {
   toDoArray.push(toDo);
 }
 
-//function for opening modal
+// function for opening modal
 function openModal() {
   const addItem = document.querySelector("#add-todo");
   const modal = document.querySelector("dialog");
@@ -25,9 +25,7 @@ function openModal() {
   });
 }
 
-openModal();
-
-//function for closing modal
+// function for closing modal
 function closeModal() {
   const closeModalbutton = document.querySelector(".cancel-button");
   const modal = document.querySelector("dialog");
@@ -37,9 +35,7 @@ function closeModal() {
   });
 }
 
-closeModal();
-
-//create a function for displaying todo from array
+// create a function for displaying todo from array
 function displayToDo() {
   const addTaskButton = document.querySelector(".add-task-button");
   const modal = document.querySelector("dialog");
@@ -68,30 +64,79 @@ function displayToDo() {
     const newToDo = new ToDo(taskTitle.value, "", false);
 
     addToDoToArray(newToDo);
-    completeToDo();
+    sortCompletedToDo(checkBox);
+    renderToDoList();
     modal.close();
   });
 }
-displayToDo();
 
-console.log(toDoArray);
-
-//function that completes todo and moves them to bottom of hr rule
+// function that completes todo and moves them to bottom of hr rule
 function completeToDo() {
-  const checkBoxes = document.querySelectorAll(".todo-checkbox");
+  const toDoContainer = document.querySelector(".need-todo");
 
-  checkBoxes.forEach((checkBox) => {
-    checkBox.addEventListener("change", function () {
-      const todoItem = checkBox.parentNode;
-
-      todoItem.remove();
-    });
+  toDoContainer.addEventListener("change", (event) => {
+    if (event.target.classList.contains("todo-checkbox")) {
+      sortCompletedToDo(event.target);
+      renderToDoList();
+    }
   });
 }
 
-//function for adding a completed to do in right section
-function sortCompletedToDo() {
-  const completedtodo = document.querySelector(".completed-todo");
-  if (checkBox.checked) {
+// function for sorting the array item to the back of the array
+function sortCompletedToDo(checkBox) {
+  const todoItem = checkBox.parentNode;
+  const taskName = todoItem.querySelector(".todo-item-title").innerText;
+
+  // Find the todo item in the array by its task name
+  const completedToDo = toDoArray.find((todo) => todo.taskName === taskName);
+
+  if (completedToDo) {
+    // Set complete property to true
+    // completedToDo.complete = true;
+
+    // Remove it from the original position
+    toDoArray.splice(toDoArray.indexOf(completedToDo), 1);
+
+    // Push it to the end
+    toDoArray.push(completedToDo);
   }
 }
+
+// function to rerender todo
+function renderToDoList() {
+  const toDoContainer = document.querySelector(".need-todo");
+
+  toDoContainer.innerHTML = "";
+
+  toDoArray.forEach((todo) => {
+    const toDo = document.createElement("div");
+    const checkBox = document.createElement("input");
+    const toDoItem = document.createElement("p");
+
+    toDo.appendChild(checkBox);
+    toDo.appendChild(toDoItem);
+
+    toDoItem.innerText = todo.taskName;
+
+    toDo.classList.add("to-do");
+    toDoItem.classList.add("todo-item-title");
+    checkBox.classList.add("todo-checkbox");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("name", "todo-item-check");
+
+    toDoContainer.appendChild(toDo);
+  });
+}
+
+// Initialization function
+function initialize() {
+  openModal();
+  closeModal();
+  displayToDo();
+  completeToDo();
+}
+
+// Call initialization function when the page loads
+document.addEventListener("DOMContentLoaded", initialize);
+
+console.log(toDoArray);
